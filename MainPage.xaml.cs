da53285dcc254a1b34e3675b05597ef392da587f
Vickry Santana <vickry.santana@gmail.com>
2022-11-28 17:36:29 -0400

    Buttons now change entry text value

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -3,6 +3,7 @@
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+    public string Count { get; set; }
 
 	public MainPage()
 	{
@@ -11,5 +12,21 @@ public partial class MainPage : ContentPage
 
     }
 
+    private void splitEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void minusButton_Clicked(object sender, EventArgs e)
+    {
+        count = count > 0 ? count-1 : 0;
+        splitEntry.Text = count.ToString();
+    }
+
+    private void plusButton_Clicked(object sender, EventArgs e)
+    {
+        count++;
+        splitEntry.Text = count.ToString();
+    }
 }
 
